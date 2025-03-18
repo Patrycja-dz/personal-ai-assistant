dbdada7a34d7f41df384b844f3ddd165ff265431
Patrycja Dziemianko <patrycja.dziemianko@eclipsegroup.co.uk>
2025-03-18 20:42:34 +0100

    Refactor chat model configuration and enhance Telegram bot message handling

--- src/config/config.js
+++ src/config/config.js
@@ -6,7 +6,7 @@ export const PORT = process.env.PORT || 5000;
 export const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN;
 export const GROQ_API_KEY = process.env.GROQ_API_KEY;
 export const CHAT_MODEL = process.env.CHAT_MODEL || "llama-3.3-70b-versatile";
-export const CHAT_TEMPERATURE = process.env.CHAT_TEMPERATURE || 0.5;
+export const CHAT_TEMPERATURE = parseFloat(process.env.CHAT_TEMPERATURE) || 0.5;
 export const CHAT_MAX_COMPLETION_TOKENS =
-  process.env.CHAT_MAX_COMPLETION_TOKENS || 1024;
-export const CHAT_TOP_P = process.env.CHAT_TOP_P || 1;
+  parseInt(process.env.CHAT_MAX_COMPLETION_TOKENS) || 1024;
+export const CHAT_TOP_P = parseFloat(process.env.CHAT_TOP_P) || 1;
--- src/services/messagehandler.service.js
+++ src/services/messagehandler.service.js
@@ -1,25 +1,32 @@
 import { logger } from "../utils/logger.js";
 
 export class MessageHandlerService {
-  constructor(bot, groqService) {
+  constructor(bot, groqService, telegramBot) {
     this.bot = bot;
     this.groqService = groqService;
+    this.telegramBotService = telegramBot;
   }
   onMessage() {
     this.bot.on("message", async (message) => {
       const { chat, text } = message;
       const chatId = chat.id;
 
-      if (!text) {
-        this.bot.sendMessage(chatId, "Sorry, I only process text messages.");
+      if (!text || text.startsWith("/")) {
         return;
       }
+
       logger.log(`Received message: ${text}`);
       try {
-        const response = await this.groqService.getGroqChatCompletion(text);
-
+        const response = await this.groqService.getGroqChatCompletion(
+          text,
+          this.telegramBotService.chatHistory
+        );
+        const botReply = response.choices?.[0]?.message?.content;
         if (response.choices?.[0]?.message?.content) {
+          logger.log(response.choices[0]);
           this.bot.sendMessage(chatId, response.choices[0].message.content);
+          this.telegramBotService.addMessageToChatHistory(text, "user");
+          this.telegramBotService.addMessageToChatHistory(botReply, "system");
         } else {
           this.bot.sendMessage(
             chatId,
